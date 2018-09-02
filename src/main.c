2390dac1e1e821e174e407d368aa73d3e8cbd35c
Aleksey Kislitsa <aleksey.kislitsa@gmail.com>
2018-09-02 13:37:57 +0300

    Has been replace tab to space in src/main and added several types of temporary files into .gitignore

--- src/main.c
+++ src/main.c
@@ -4,23 +4,23 @@ void SysTick_Handler(void) {
   static uint16_t tick = 0;
 
   switch (tick++) {
-  	case 100:
-  		tick = 0;
-  		GPIOC->ODR ^= (1 << 8);
-  		break;
+    case 100:
+        tick = 0;
+        GPIOC->ODR ^= (1 << 8);
+        break;
   }
 }
 
 int main(void)
 {
 
-	RCC->AHBENR |= RCC_AHBENR_GPIOCEN; 	// enable the clock to GPIOC
-						//(RM0091 lists this as IOPCEN, not GPIOCEN)
+    RCC->AHBENR |= RCC_AHBENR_GPIOCEN;  // enable the clock to GPIOC
+                        //(RM0091 lists this as IOPCEN, not GPIOCEN)
 
-	GPIOC->MODER = (1 << 16);
+    GPIOC->MODER = (1 << 16);
 
-	SysTick_Config(SystemCoreClock/100);
+    SysTick_Config(SystemCoreClock/100);
 
-	while(1);
+    while(1);
 
 }
