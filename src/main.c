61d3210ae1c52d3bbea1c576667ae7ebd49a9c33
Aleksey Kislitsa <aleksey.kislitsa@gmail.com>
2018-09-15 20:02:47 +0300

    Has been added second led for blinking

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@ void SysTick_Handler(void) {
   switch (tick++) {
     case 100:
         tick = 0;
-        GPIOC->ODR ^= (1 << 8);
+        GPIOC->ODR ^= (1 << 8) | (1 << 9);
         break;
   }
 }
@@ -16,7 +16,8 @@ int main(void)
     RCC->AHBENR |= RCC_AHBENR_GPIOCEN;  // enable the clock to GPIOC
                         //(RM0091 lists this as IOPCEN, not GPIOCEN)
 
-    GPIOC->MODER = (1 << 16);
+    GPIOC->MODER = (1 << 16) | (1 << 18);
+    GPIOC->ODR |= (1 << 8);
 
     SysTick_Config(SystemCoreClock/100);
 
